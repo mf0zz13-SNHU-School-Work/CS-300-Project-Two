3710c7bc7fd25411cd3f2de681f44fb76f9a8957
mf0zz13 <michael.foster31@outlook.com>
2025-06-20 21:53:51 -0400

    Added intial menu and struct - Added menu without functionality - Created the Course struct

--- ABCU Advising Program/ABCU Advising Program.cpp
+++ ABCU Advising Program/ABCU Advising Program.cpp
@@ -1,20 +1,87 @@
-// ABCU Advising Program.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//============================================================================
+// Name        : ABCU Advising Program.cpp
+// Author      : Michael Foster
+// Version     : 1.0
+// Description : CS 300 Project Two
+//============================================================================
 
 #include <iostream>
+#include<vector>
+
+using namespace std;
+
+/*
+	@brief Course is an object that holds a record of a course.
+	@details Course object contains courseNumber, courseName, and prerequisites required to take the course.
+*/
+struct Course {
+private:
+	string courseNumber;
+	string courseName;
+	vector<string> preReqs;
+public:
+	/*
+		@brief Set the course number for a course object.
+		@param courseNumber A string to be stored as the course objects course number.
+	*/
+	void SetCourseNumber(string courseNumber) {
+		this->courseNumber = courseNumber;
+	}
+
+	/*
+		@brief Set the course name for a course object.
+		@param courseName A string to be stored as the course objects course name.
+	*/
+	void SetCourseName(string courseName) {
+		this->courseName = courseName;
+	}
+
+	/*
+		@brief Set the prerequisite for a course object. Parameter passed is added to the course objects preReqs vector.
+		@param courseNumber A string to be stored as the course objects course number.
+	*/
+	void SetPreReq(string courseName) {
+		this->preReqs.push_back(courseName);
+	}
+
+	Course() {
+		courseNumber = "unknown";
+		courseName = "unknown";
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	bool exit = false;
+	int userInput = 0;
+
+	do
+	{
+		cout << "1. Load Data Structure." << endl;
+		cout << "2. Print Course List." << endl;
+		cout << "3. Print Course." << endl;
+		cout << "9. Exit.\n" << endl;
+		cout << "What would you like to do? ";
+		cin >> userInput;
+
+		switch (userInput) {
+		case 1:
+			// FIXME: Add Method to add data
+			break;
+		case 2:
+			// FIXME: Add Method To Print Course List
+			break;
+		case 3:
+			// FIXME: Add Method To Print Course
+			break;
+		case 9:
+			exit = true;
+			cout << "Thank you for using the course planner!" << endl;
+			break;
+		default:
+			cout << userInput << " is not a valid option.\n" << endl;
+		}
+
+	} while (!exit);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
